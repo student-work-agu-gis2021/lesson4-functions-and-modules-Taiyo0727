b3d70eee5285fe8ae6562c731a2532e2b192a32d
Taiyo0727 <>
2021-04-30 03:12:59 +0000

    I added a docstring to the script file.

--- temp_functions.py
+++ temp_functions.py
@@ -1,7 +1,12 @@
  def fahr_to_celsius(temp_fahrenheit):
+  #We can change from fahrenheit to fahr_to_celsius.
+  #temp_fahrenheit : the temparture you want to change.
   converted_temp = (temp_fahrenheit -32) /1.8
   return converted_temp
 def temp_classifier(temp_celsius):
+#purpose of the function : separate to temperature
+#parameter : temperature of Celsius
+#returned values : x < -2 is 0, -2<=x <2 is 1, 2<= x <15 is 2, and x>=15 is 3
   if temp_celsius < -2:
     return 0
   elif temp_celsius <2:
